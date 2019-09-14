d3ab383592a3cfc575832c77d38a5457fcb8e4c9
srstr <s.r.strachan@outlook.com>
2019-09-14 12:05:10 +0100

    Random selector

--- selector.scala
+++ selector.scala
@@ -1,3 +1,15 @@
-object Selector extends App{
-  println("hello world")
+import scala.util.Random
+
+object MainSelector extends App{
+  chooseActivity()
+
+  def chooseActivity () ={
+    var activites=Array("Piano", "Choir", "Angular")
+    val rand = new Random()
+    val number = rand.nextInt(3)
+    //println(number+"\n")
+    var activity=activites(number)
+    print(activity+"\n")
+  }
 }
+
