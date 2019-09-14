fd66da382431e085238cf9a4160fff55e6764f8c
srstr <s.r.strachan@outlook.com>
2019-09-14 21:19:19 +0100

    basic week scheduler

--- selector.scala
+++ selector.scala
@@ -1,15 +1,27 @@
 import scala.util.Random
 
-object MainSelector extends App{
-  chooseActivity()
+object MainScheduler extends App{
+  val rand = new Random()
+  val maxHoursWeekdays: Integer = 6
+  val maxHoursWeekends: Integer = 6
+  val weekdays : Seq[String]=Seq("Monday","Tuesday","Wednesday","Thursday","Friday")
+  val weekends : Seq[String]=Seq("Saturday", "Sunday")
+  for (i <- 0 until 5)
+    println(weekdays(i)+": "+chooseActivity(rand)+" for "+chooseTime(rand)+" hours")
+  for(i <- 0 until 2)
+    println(weekends(i)+": "+chooseActivity(rand)+" for "+chooseTime(rand)+" hours")
+  def chooseActivity (rand: Random):String ={
+    val activites:Seq[String]=Seq("Piano", "Choir", "Angular", "Scala")
+    val number = rand.nextInt(activites.size) //vals are immutable, vars are mutable
+    return activites(number)
+  }
 
-  def chooseActivity () ={
-    var activites=Array("Piano", "Choir", "Angular")
-    val rand = new Random()
-    val number = rand.nextInt(3)
-    //println(number+"\n")
-    var activity=activites(number)
-    print(activity+"\n")
+  def chooseTime (rand : Random): Double  ={
+    val times:Seq[Double]=Seq(0.5,1,2,3)
+    var index=rand.nextInt(times.size)
+    return times(index)
   }
 }
+//plans out whole timetable, one activity each day 7 days a week for a specified amount of timne
+//weekdays will have a smaller set of times than weekends
 
