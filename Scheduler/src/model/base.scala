00e0628b019218714316da592ff36fa4b15a1799
srstr <s.r.strachan@outlook.com>
2019-09-15 13:47:53 +0100

    project properties

---
+++ Scheduler/src/model/base.scala
@@ -0,0 +1,10 @@
+package model
+
+object Days extends Enumeration {
+  type Day=Value
+  val mo = Value("Monday")
+  val tu = Value("Tuesday")
+  val we = Value("Wednesday")
+  val th = Value("Thursday")
+  val fr = Value("Friday")
+}
